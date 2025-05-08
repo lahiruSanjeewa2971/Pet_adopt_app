a6945cefd0e138b93d8c0b4c2015f695306f1862
lahiruSanjeewa2971 <rathne1997@gmail.com>
2025-05-08 20:29:43 +0530

    first commit

---
+++ pet_adopt_app/app/_layout.jsx
@@ -0,0 +1,46 @@
+import * as SecureStore from "expo-secure-store";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was userd.`);
+      } else {
+        console.log("No values stored under key :", key);
+      }
+      return item;
+    } catch (error) {
+      console.log("SecureStore get item error :", error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (error) {
+      return;
+    }
+  },
+};
+
+export default function RootLayout() {
+  useFonts({
+    delius: require("../assets/fonts/Delius-Regular.ttf"),
+  });
+
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login/index" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
+  );
+}
---
+++ pet_adopt_app/app/login/index.jsx
@@ -0,0 +1,98 @@
+import { Image, Pressable, Text, View } from "react-native";
+import Colors from "../../constants/Colors";
+import { useCallback, useEffect } from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as Linking from "expo-linking";
+import { useAuth } from "@clerk/clerk-expo";
+import * as AuthSession from "expo-auth-session";
+import { useSSO } from "@clerk/clerk-expo";
+
+export const useWarmUpBrowser = () => {
+  useEffect(() => {
+    // Preloads the browser for Android devices to reduce authentication load time
+    // See: https://docs.expo.dev/guides/authentication/#improving-user-experience
+    void WebBrowser.warmUpAsync();
+    return () => {
+      // Cleanup: closes browser when component unmounts
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+};
+
+// Handle any pending authentication sessions
+WebBrowser.maybeCompleteAuthSession();
+
+export default function LoginScreen() {
+  useWarmUpBrowser();
+  const { startSSOFlow } = useSSO();
+
+  const onPress = useCallback(async () => {
+    try {
+      // Start the authentication process by calling `startSSOFlow()`
+      const { createdSessionId, setActive, signIn, signUp } =
+        await startSSOFlow({
+          strategy: "oauth_google",
+          // For web, defaults to current path
+          // For native, you must pass a scheme, like AuthSession.makeRedirectUri({ scheme, path })
+          // For more info, see https://docs.expo.dev/versions/latest/sdk/auth-session/#authsessionmakeredirecturioptions
+          //   redirectUrl: AuthSession.makeRedirectUri(),
+          redirectUrl: Linking.createURL("/home", { scheme: "myapp" }),
+        });
+
+      // If sign in was successful, set the active session
+      if (createdSessionId) {
+      } else {
+        // If there is no `createdSessionId`,
+        // there are missing requirements, such as MFA
+        // Use the `signIn` or `signUp` returned from `startSSOFlow`
+        // to handle next steps
+      }
+    } catch (err) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, []);
+
+  return (
+    <View style={{ backgroundColor: Colors.WHITE, height: "100%" }}>
+      <Image
+        source={{
+          uri: "https://plus.unsplash.com/premium_vector-1721985703466-0e6a7f83486a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nfGVufDB8fDB8fHww",
+        }}
+        style={{ width: "100%", height: 500 }}
+      />
+      <View style={{ padding: 20, display: "flex", alignItems: "center" }}>
+        <Text
+          style={{ fontFamily: "delius", fontSize: 20, textAlign: "center" }}
+        >
+          Ready to make a new friend?
+        </Text>
+        <Text style={{ fontSize: 15, textAlign: "center", color: Colors.GRAY }}>
+          Let's adopt the pet which you like and make there life happy again.
+        </Text>
+
+        <Pressable
+          onPress={onPress}
+          style={{
+            padding: 14,
+            marginTop: 100,
+            backgroundColor: Colors.PRIMARY,
+            width: "100%",
+            borderRadius: 14,
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "delius",
+              fontSize: 20,
+              textAlign: "center",
+            }}
+          >
+            Get started
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
