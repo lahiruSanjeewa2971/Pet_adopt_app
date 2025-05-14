04eaa2f7710468f08da17ba04f10b84527758e37
lahiruSanjeewa2971 <rathne1997@gmail.com>
2025-05-14 23:38:18 +0530

    Single pet screen - added a button additionally at the bottom of the screen

--- pet_adopt_app/app/pet-details/index.jsx
+++ pet_adopt_app/app/pet-details/index.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import PetInfo from "../../components/PetDetails/PetInfo";
 import PetSubInfo from "../../components/PetDetails/PetSubInfo";
 import AboutPet from "../../components/PetDetails/AboutPet";
 import OwnerInfo from "../../components/PetDetails/OwnerInfo";
+import Colors from "../../constants/Colors";
 
 export default function PetDetails() {
   const petInfo = useLocalSearchParams();
@@ -35,6 +36,11 @@ export default function PetDetails() {
       </ScrollView>
 
       {/* adopt me button  */}
+      <View style={{position: 'absolute', width: '100%', bottom: 0}}>
+        <TouchableOpacity style={{padding: 15, backgroundColor: Colors.PRIMARY}}>
+          <Text style={{textAlign: 'center', fontFamily: 'delius', fontSize: 20}}>Adopt Me</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
